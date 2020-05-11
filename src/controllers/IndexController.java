c879abacc2458a8f210d9061a7aed24bf6872c85
cuongnguyen <chicuongnguyenit@gmail.com>
2020-05-10 22:18:51 -0500

    complete login page

--- src/controllers/IndexController.java
+++ src/controllers/IndexController.java
@@ -1,16 +1,11 @@
 package controllers;
 
-import models.UserModel;
-import services.UserService;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.ConnectException;
-import java.util.List;
 
 @WebServlet(name = "IndexController")
 public class IndexController extends HttpServlet {
